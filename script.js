e24bb24afccb4896a6f177d2ed7449e7e1ef268f
MLubeckis <185762906+MLubeckis@users.noreply.github.com>
2025-03-14 18:01:00 +0200

    Add random color to background

--- script.js
+++ script.js
@@ -7,21 +7,27 @@ function createGrid(x) {
     function createCell(x) {
         const cell = document.createElement("div");
         cell.setAttribute("class", "square");
+        cell.style.opacity = 0;
+        cell.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
         cell.style.width = `${480 / x}px`
         cell.style.height = `${480 / x}px`
         cell.addEventListener("mouseenter", function(){
-            cell.style.backgroundColor = "#b1b1b1";
-            cell.style.transition = "0s ease";
+            const opacity = cell.style.opacity;
+            cell.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+            cell.style.opacity = Number(opacity) + 0.1;
+            // cell.style.transition = "3s ease";
         })
-        cell.addEventListener("mouseleave", function(){
-            cell.style.backgroundColor = "transparent";
-            cell.style.transition = "1s ease";
-        })
+        // cell.addEventListener("mouseleave", function(){
+        //     cell.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+        //     cell.style.transition = "1s ease";
+        // })
         //cell.textContent = content;
         return cell;
     }
 
-
+    function random(number) {
+        return Math.floor(Math.random() * (number+1));
+      }
 
 
     for (let i = 0; i<x; i++) {
