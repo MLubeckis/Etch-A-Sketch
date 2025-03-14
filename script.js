412121f03cbf225caf985a281fea40991d97f15a
MLubeckis <185762906+MLubeckis@users.noreply.github.com>
2025-03-14 17:18:28 +0200

    Added function to change the grid size

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 /** @type {HTMLDivElement} */
 
-function createGrid() {
+function createGrid(x) {
     const container = document.getElementById("container");
+    container.innerHTML = ""
 
-
-    function createCell() {
+    function createCell(x) {
         const cell = document.createElement("div");
         cell.setAttribute("class", "square");
+        cell.style.width = `${480 / x}px`
+        cell.style.height = `${480 / x}px`
         cell.addEventListener("mouseenter", function(){
             cell.style.backgroundColor = "#b1b1b1";
             cell.style.transition = "0s ease";
@@ -21,10 +23,11 @@ function createGrid() {
 
 
 
-    for (let i = 0; i<16; i++) {
-        container.appendChild(createCell());
-        for (let j = 0; j<15; j++){
-            container.appendChild(createCell()); 
+
+    for (let i = 0; i<x; i++) {
+        container.appendChild(createCell(x));
+        for (let j = 0; j<x-1; j++){
+            container.appendChild(createCell(x)); 
         }
     }
 
@@ -33,4 +36,13 @@ function createGrid() {
 
 }
 
-createGrid();
+createGrid(16);
+
+document.querySelector("#setGridSize").addEventListener("click", setGridSize);
+
+function setGridSize() {
+    let size = Number(prompt("Enter the grid size you want"));
+    if (size <= 100) {
+    createGrid(size);
+    } else {alert("Too Large, Will crash!")}
+}
